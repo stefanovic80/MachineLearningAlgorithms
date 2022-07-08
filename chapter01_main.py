ac4360f530e8e346e656126455c39f27a4fbd99c
stefanovic80 <stefanovic80@ucdconnect.ie>
2022-07-08 16:35:57 +0200

    few further improvements

--- chapter01_main.py
+++ chapter01_main.py
@@ -18,14 +18,17 @@ df = pd.read_excel(fileName)#, header = None)
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
-plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
-plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'x', label = 'versicolor')
+plt.scatter(X[:50, 0], X[:50, 1], \
+            color = 'red', marker = 'o', label = 'setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], \
+            color = 'blue', marker = 'x', label = 'versicolor')
 plt.xlabel('sepal length')
 plt.ylabel('petal length')
 plt.legend(loc= 'upper left')
 plt.grid()
 plt.show()
 
+#%run path + '/' + perceptron.py
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, y)
 plt.grid()
@@ -60,10 +63,10 @@ def plot_decision_regions(X, y, classifier, resolution = 0.02):
         plt.scatter(x = X[ y == c1, 0 ], y = X[ y == c1, 1 ], \
                     alpha = 0.8, c = cmap(idx), \
                     marker = markers[idx], label = c1)
-plt.figure()       
+plt.figure( figsize = (8, 4))       
 plot_decision_regions(X, y, classifier = ppn)
 plt.xlabel('sepal length [cm]')#labels
 plt.ylabel('petal length [cm]')
 plt.legend(loc = 'upper left')
+plt.grid()
 plt.show()
-    
